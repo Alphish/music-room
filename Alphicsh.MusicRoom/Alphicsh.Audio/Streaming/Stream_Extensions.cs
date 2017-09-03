9f697bc42a1c489f0ee2f117e6cadd656cd0bf3a
Alice Jankowska <alphish.creature@gmail.com>
2017-09-03 23:22:52 +0200

    Creep: Added the LoopStream class, the WavePlayer class and the TrackLoader static class
    The LoopStream class allows looping a stream between arbitrary points for a specified or unlimited number of repetitions. The range of the playable track in the stream can be changed as well.
    The WavePlayer class provides a basic wave stream playback functionality.
    The TrackLoader static class provides a convenient way to load a file based on its extension.
    
    Also, VorbisWaveReader has been copied from NAudio.Vorbis source and had its Length and Position properties modified so that they'd work with the LoopStream correctly.

---
+++ Alphicsh.MusicRoom/Alphicsh.Audio/Streaming/Stream_Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphicsh.Audio.Streaming
+{
+    /// <summary>
+    /// Provides extension methods for various streams.
+    /// </summary>
+    public static class Stream_Extensions
+    {
+        /// <summary>
+        /// Attempts to read a specific number of bytes from the given stream, until no more bytes can be read.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <param name="buffer">The bytes buffer to copy the stream contents to.</param>
+        /// <param name="offset">The position where the bytes should be copied.</param>
+        /// <param name="count">The number of bytes requested.</param>
+        /// <returns>The number of bytes actually read.</returns>
+        public static int ReadMax(this Stream stream, byte[] buffer, int offset, int count)
+        {
+            int result = 0;
+            int read;
+            while (count > 0 && (read = stream.Read(buffer, offset, count)) != 0)
+            {
+                result += read;
+                offset += read;
+                count -= read;
+            }
+            return result;
+        }
+    }
+}
---
+++ Alphicsh.MusicRoom/Alphicsh.Audio/Streaming/TrackLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using NAudio.Wave;
+//using NAudio.Vorbis;
+using NAudio.Flac;
+
+namespace Alphicsh.Audio.Streaming
+{
+    /// <summary>
+    /// Provides functionality of loading soundtracks to their basic stream or to memory.
+    /// </summary>
+    public static class TrackLoader
+    {
+        #region Format-based streams
+
+        /// <summary>
+        /// Provides the most appropriate wave stream for a given track.
+        /// </summary>
+        /// <param name="path">The path of the track to stream.</param>
+        /// <returns>The wave stream for the given track.</returns>
+        public static WaveStream LoadStream(string path)
+        {
+            var ext = Path.GetExtension(path);
+            switch (ext)
+            {
+                case ".flac":
+                    return new FlacReader(path);
+                case ".mp3":
+                    return new Mp3FileReader(path);
+                case ".ogg":
+                    return new VorbisWaveReader(path);
+                case ".wav":
+                    return new WaveFileReader(path);
+                default:
+                    throw new NotSupportedException($"Extension {ext} is not supported.");
+            }
+        }
+
+        #endregion
+    }
+}
